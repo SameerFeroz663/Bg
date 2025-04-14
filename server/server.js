b11a7db4fc8b74166cf24a0204ad80f00be426a1
Shadow <darth3157@gmail.com>
2025-04-15 00:43:52 +0500

    1

--- server/server.js
+++ server/server.js
@@ -20,38 +20,18 @@ app.listen(PORT, ()=> console.log("Server Running on port "+PORT))*/
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
-import connectDB from './configs/mongodb.js'
+import connectDB from '../configs/mongodb.js'
+import serverless from 'serverless-http'
 
-// App Config
 const app = express()
 
-// Middleware
 app.use(express.json())
 app.use(cors())
 
-// Routes
 app.get('/', (req, res) => res.send("API Working"))
 
-// Connect to DB
-;(async () => {
-    try {
-        await connectDB()
-    } catch (err) {
-        console.error("Failed to connect DB", err)
-    }
-})()
+await connectDB()
 
-// Export handler for Vercel
-module.exports = (req, res) => {
-  app(req, res)
-}
+const handler = serverless(app)
 
-// Optional local dev
-if (process.env.NODE_ENV !== 'production') {
-    const PORT = process.env.PORT || 4000
-    app.listen(PORT, () => {
-        console.log("Local server running on port", PORT)
-    })
-}
-
-  
+export default handler
