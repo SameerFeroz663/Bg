353360c6e6146f7b1e2e533aa1a32e85509eadac
Shadow <darth3157@gmail.com>
2025-04-15 00:05:38 +0500

    changes to server and vercel

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import 'dotenv/config'
+/*import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import connectDB from './configs/mongodb.js'
@@ -16,4 +16,25 @@ app.use(cors())
 // API route
 app.get('/',(req,res)=> res.send("API Working"))
 
-app.listen(PORT, ()=> console.log("Server Running on port "+PORT))
+app.listen(PORT, ()=> console.log("Server Running on port "+PORT))*/
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import connectDB from './configs/mongodb.js'
+import serverless from 'serverless-http'
+
+// App Config
+const app = express()
+
+// Middleware
+app.use(express.json())
+app.use(cors())
+
+// Routes
+app.get('/', (req, res) => res.send("API Working"))
+
+// Connect to DB
+await connectDB()
+
+// Export handler for Vercel
+export const handler = serverless(app)
