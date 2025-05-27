8d48f607c36b97a4a4f5e9d0f5416cead6480e8a
SameerFeroz663 <ferozsameer07+dev@gmail.com>
2025-05-28 00:47:09 +0500

    Add files via upload

---
+++ configs/mongodb.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+  const uri = process.env.MONGO_URI
+  if (!uri) throw new Error("MONGO_URI not defined")
+
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+
+  console.log("✅ MongoDB connected")
+}
+
+export default connectDB
