678cd98b79a6e1d26ff6406be87f53c37d010876
SameerFeroz663 <ferozsameer07+dev@gmail.com>
2025-05-29 01:35:06 +0500

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { assets } from '../assets/assets';
 
-const Steps = () => {
+const Header = () => {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
 
