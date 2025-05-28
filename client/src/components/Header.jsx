cf3689be06b9d8f2fb118a04690e71f36ef8cc62
SameerFeroz663 <ferozsameer07+dev@gmail.com>
2025-05-29 01:32:53 +0500

    Update Header.jsx

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -61,7 +61,7 @@ const Steps = () => {
             </h1>
             <p className='my-6 text-[15px] text-gray-500'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br className='max-sm:hidden'/>Lorem Ipsum has been the industry's standard dummy text ever.</p>
             <div>
-                <input type="file" name="" id="upload1" hidden/>
+                <input type="file" name="" id="upload1" hidden onChange={handleImageUpload}/>
                 <label className='inline-flex gap-3 px-8 py-3.5 rounded-full cursor-pointer bg-gradient-to-r from-violet-600 to-fuchsia-500 m-auto hover:scale-105 transition-all duration-700' htmlFor="upload1">
                     <img width={20} src={assets.upload_btn_icon} alt="" />
                     <p className='text-white text-sm'>Upload your image</p>
